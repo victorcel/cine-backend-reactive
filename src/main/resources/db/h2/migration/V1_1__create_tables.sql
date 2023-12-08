a028939a14e4843dcd0c8c73690a65c30bd40458
Victor Elias Barrera Florez <victorcel2005@gmail.com>
2023-12-08 15:30:02 -0500

    refactor: ajustes de mejora

--- src/main/resources/db/h2/migration/V1_1__create_tables.sql
+++ src/main/resources/db/h2/migration/V1_1__create_tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE movies
 (
-    id          uuid          NOT NULL PRIMARY KEY,
-    title       varchar(100)  not null,
+    id          uuid PRIMARY KEY,
+    title       varchar(100)   not null,
     director    varchar(100)  not null,
     genre       varchar(100)  not null,
-    year        int           not null,
-    description varchar(1000) not null,
-    image       varchar(1000) not null,
+    release_year        DECIMAL(4, 0)  not null,
+    description varchar(255)   not null,
+    image       varchar(255)   not null,
     language    varchar(100)  not null,
-    prices      int           not null,
+    prices      DECIMAL(10, 2) not null,
     released    varchar(100)  not null,
     imdb_rating  varchar(100)  not null,
-    createdAt   timestamp     not null
+    created_at  DATE           not null
 );
 
 CREATE TABLE showtime
@@ -22,7 +22,6 @@ CREATE TABLE showtime
     showtime  varchar(100) NOT NULL,
     price     int          NOT NULL,
     created_at timestamp    NOT NULL
-)
+);
 
-ALTER TABLE showtime ADD PRIMARY KEY (id);
 ALTER TABLE showtime ADD FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE CASCADE;
---
+++ src/main/resources/db/h2/migration/V1_2__insert_movies.sql
@@ -0,0 +1,28 @@
+INSERT INTO movies (
+                    id,
+                    title,
+                    director,
+                    genre,
+                    release_year,
+                    description,
+                    image,
+                    language,
+                    prices,
+                    released,
+                    imdb_rating,
+                    created_at
+)
+VALUES (
+        '49e5497e-5914-472e-aae4-1dbf5c9d049e',
+        'The Fast and the Furious',
+        'Rob Cohen',
+        'Action, Crime, Thriller',
+        2002,
+        'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to destroy.',
+        'https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+        'English, Spanish',
+        10000,
+        '22 Jun 2001',
+        '6.8',
+        '2023-12-08'
+       );
